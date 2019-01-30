71ec3047dd67fd6ba2598b5586251bea6a8a6d01
Josh York <joshua.s.york@gmail.com>
2019-01-29 23:30:36 -0600

    add db connection logs. update config values

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,11 +3,27 @@ import config from 'config';
 
 import createDocumentInstances from './collections/documents';
 
+const url = process.env.DB_URL || config.get('arango.url');
+const dbName = process.env.DB_NAME || config.get('arango.dbName');
+const username = process.env.DB_USERNAME || config.get('arango.username');
+const password = process.env.DB_PASSWORD || config.get('arango.password');
+
+console.log(
+  `Connecting to DB.\n\tenv=${process.env.NODE_ENV},\n\tURL: `,
+  url,
+  '\n\tdb: ',
+  dbName,
+  '\n\tusername: ',
+  username,
+);
+
 const db = new Database({
-  url: config.get('arango.url'),
+  url,
 });
-db.useDatabase(config.get('arango.db'));
-db.useBasicAuth(config.get('arango.username'), config.get('arango.password'));
+db.useDatabase(dbName);
+db.useBasicAuth(password);
+
+console.log('Arango connection established');
 
 export default {
   db,
